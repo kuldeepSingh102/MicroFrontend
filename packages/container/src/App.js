c129369a6e871d570f8b92bd6f5cc1102cd3a4bd
Kuldeep Singh <ks2sept2005ks@gmail.com>
2025-06-18 13:23:05 +0530

    css fixed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Header from "./components/Header"; 
-import MarketingApp from "./components/MarketingApp"; 
+import Header from "./components/Header";
+import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default () => {
   return (
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
